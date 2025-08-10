6ecff81f1aa73d4de70a377736dda3d7f87d316c
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-10 13:33:24 +0500

    Create sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max) || min < 1 || max < 1)
+        return "ERROR"
+    if (min > max){
+        const temp = min
+        min = max
+        max = temp
+    }
+    let total = 0
+    while (min <= max){
+        total += min
+        min ++
+    }
+    return total
 };
 
 // Do not edit below this line
