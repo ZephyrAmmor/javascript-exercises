7ff9c01ae668d290123b44cc96dd34aa858dac55
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-11 18:42:36 +0500

    Complete 12 calculator problem

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(item) {
+  return  item.reduce((sum, num) => sum + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(items) {
+  return items.reduce((multiple, num) => multiple * num, 1)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let factor = 1
+  if (x == 0)
+    return 1;
+	else
+  {
+    for(let i = 1; i <= x; i ++){
+    factor *= i
+  }
+  return factor
+}
 };
 
 // Do not edit below this line
