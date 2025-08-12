f634f0430f625149e73b1e6797bf2bbd02a3b6b6
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-12 09:49:45 +0500

    13 palindromes done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    let arr = str.split('').filter(item => {
+        if (item !== ' ' && item !== ',' && item !== '.' && item!== '!')
+            return item
+    })
+    console.log(arr)
+    let copyArr = arr
+    for(let i = 0, j = copyArr.length - 1; i < arr.length, j >= 0; i++, j--){
+        if (arr[i] !== copyArr[j])
+            return false;
+    }
+    return true
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+// This solutions is like I'm in C
