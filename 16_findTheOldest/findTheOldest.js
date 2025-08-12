93873dc70c474b4b37fbb5e8cb48b878e370a8b7
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-12 10:48:23 +0500

    complete findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peoples) {
+    for(people of peoples){
+        if (people.yearOfDeath == undefined){
+            const today = new Date()
+            people.alive = today.getFullYear()
+            console.log(people.alive, today, today.getFullYear())
+        }
+    }
+    let arrayByAge = peoples.sort((first, second) => {
+        const firstAge = (first.yearOfDeath ||first.alive) - first.yearOfBirth
+        const secondAge = (second.yearOfDeath || second.alive) - second.yearOfBirth
+        return firstAge - secondAge
+    })
+    return arrayByAge.at([-1])
 };
 
 // Do not edit below this line
