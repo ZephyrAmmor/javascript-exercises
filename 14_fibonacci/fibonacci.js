2287d4374e1b5cc34173ae45cb718f3760192160
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-12 10:22:50 +0500

    14 fibonacci done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num < 0)
+        return "OOPS"
+    if (num == 0)
+        return 0
+    let result = 1
+    let prevResult = 0
+    for(let i = 1; i < num; i++){
+        const temp = result 
+        result += prevResult
+        prevResult = temp
+    }
+    return result
 };
 
 // Do not edit below this line
